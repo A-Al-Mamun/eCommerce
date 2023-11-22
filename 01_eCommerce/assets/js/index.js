2ba06ad4d606ec69ee385c886b14301b13e9787c
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-11-22 23:50:55 +0600

    Edit menu hover effect color

--- 01_eCommerce/assets/js/index.js
+++ 01_eCommerce/assets/js/index.js
@@ -1,9 +1,4 @@
 
-let welcome = "Welcome To eCommerce Plateforme";
-welcome.style.fontSize = '28px';
-welcome.style.color = 'red';
-
-alert(welcome);
 // Nav Bar
 const cat = () => {
     var barMenu = document.getElementById('barMenu');
