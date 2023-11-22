50bacea30bcc41f69becf67c4a1554a65633f0f0
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-11-22 23:41:20 +0600

    Home tag create in login page, and some direction changes

--- 01_eCommerce/assets/js/index.js
+++ 01_eCommerce/assets/js/index.js
@@ -1,3 +1,9 @@
+
+let welcome = "Welcome To eCommerce Plateforme";
+welcome.style.fontSize = '28px';
+welcome.style.color = 'red';
+
+alert(welcome);
 // Nav Bar
 const cat = () => {
     var barMenu = document.getElementById('barMenu');
