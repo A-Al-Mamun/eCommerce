28bdb301a86e0e6c6a655dae5b1f0bccbd7e597c
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-11-24 00:35:19 +0600

    Try to fix the login function

--- 01_eCommerce/assets/js/login.js
+++ 01_eCommerce/assets/js/login.js
@@ -1,34 +1,29 @@
 // LogIn Section
 
-let logMain = document.getElementsByClassName('log')[0];
-// let mn = document.getElementsByClassName("log-btn")[0];
+let logBtn = document.getElementsByClassName("log-btn")[0];
+// let logMain = document.getElementsByClassName('log')[0];
 
 // console.log(logBtn);
 // alert(logMain);
 
 // alert(logMain.innerHTML = 'Admin');
-// if (mn !== undefined && mn !== null) {
-    // alert(mn.innerHTML = 'Hello');
-  
-const mn = () => {
+logBtn.addEventListener('click', () => {
     let email = document.getElementById('log-email').value;
     let pass = document.getElementById('log-pass').value;
+    
 
-
-    let userEmail = "admin@gmail.com";
+    let userEmail = "admin@gmail.com"; 
     let userPass = "admin";
-
-
-    if ((userEmail == email) && (userPass == pass)) {
-        alert("Seccessfully LogIn");
+    
+    
+    if((userEmail == email) && (userPass == pass)){
+        alert("Seccessfully LogIn"); 
         window.location.assign("/01_HTML_CSS_JS/eCommerce/01_eCommerce/index.html");
-        logMain.innerHTML = 'Admin';
-    } else {
+        // logMain.innerHTML = 'Admin'; 
+    }else{
         alert('Not matched');
     }
 
-}
+});
 
-// }
 
-//         alert(logMain.innerHTML = 'Admin');
