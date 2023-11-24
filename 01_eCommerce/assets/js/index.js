122b95ee484d42c7dd76799e3305061f9afb5e07
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-11-24 23:30:28 +0600

    Slide design and add some picture

--- 01_eCommerce/assets/js/index.js
+++ 01_eCommerce/assets/js/index.js
@@ -12,14 +12,7 @@ const cat = () => {
 }
 
 
-// Register Section
-// let reg = document.getElementsByClassName('register')[0];
-// reg.addEventListener('click', () => {
-//     let name = document.getElementById('name').value;
-//     let email = document.getElementById('email').value;
-//     let phone = document.getElementById('phone').value;
-//     let pass = document.getElementById('pass').value;
 
-//     // alert(name + email + phone + pass);
-// })
+// Slide Section 
+
 
