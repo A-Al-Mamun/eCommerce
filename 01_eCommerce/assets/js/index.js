0ddceba6fbb816117e5987e11f559e5596b20f5c
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-11-24 23:46:51 +0600

    Now add Functionality by js in slide section

--- 01_eCommerce/assets/js/index.js
+++ 01_eCommerce/assets/js/index.js
@@ -15,4 +15,29 @@ const cat = () => {
 
 // Slide Section 
 
+let slidePre = document.getElementsByClassName('slide-pre')[0];
+let slideNext = document.getElementsByClassName('slide-next')[0];
 
+let slider = document.getElementsByClassName('slider')[0];
+let sliderItem = document.querySelectorAll('.slider-item');
+
+let slideIndex = 0;
+sliderItem[slideIndex].classList.add('active');
+
+slidePre.addEventListener('click', () => {
+    sliderItem[slideIndex].classList.remove('active');
+
+    slideIndex = (slideIndex == 0) ? sliderItem.length - 1 : slideIndex - 1;
+    sliderItem[slideIndex].classList.add('active');
+
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+})
+
+slideNext.addEventListener('click', () => {
+    sliderItem[slideIndex].classList.remove('active');
+
+    slideIndex = (slideIndex == sliderItem.length - 1) ? 0 : slideIndex + 1;
+    sliderItem[slideIndex].classList.add('active');
+
+    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+})
