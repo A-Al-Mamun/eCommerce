fe676d6a1998b3d56a65753dc14139c9560ad646
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2023-11-25 00:01:03 +0600

    Try to complete the extra style on the slide

--- 01_eCommerce/assets/js/index.js
+++ 01_eCommerce/assets/js/index.js
@@ -21,23 +21,41 @@ let slideNext = document.getElementsByClassName('slide-next')[0];
 let slider = document.getElementsByClassName('slider')[0];
 let sliderItem = document.querySelectorAll('.slider-item');
 
+
+let sliderInfo = document.getElementsByClassName('slider-info')[0];
+let sliderInfoItem = document.querySelectorAll('.slider-info-item');
+
 let slideIndex = 0;
 sliderItem[slideIndex].classList.add('active');
+sliderInfoItem[slideIndex].classList.add('active');
 
 slidePre.addEventListener('click', () => {
     sliderItem[slideIndex].classList.remove('active');
 
+    sliderInfoItem[slideIndex].classList.remove('active');
+
     slideIndex = (slideIndex == 0) ? sliderItem.length - 1 : slideIndex - 1;
     sliderItem[slideIndex].classList.add('active');
 
+    sliderInfoItem[slideIndex].classList.add('active');
+
     slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+
+    sliderInfo.style.transform = `translateX(-${slideIndex * 100}%)`;
+
 })
 
 slideNext.addEventListener('click', () => {
     sliderItem[slideIndex].classList.remove('active');
 
+    sliderInfoItem[slideIndex].classList.remove('active');
+
     slideIndex = (slideIndex == sliderItem.length - 1) ? 0 : slideIndex + 1;
     sliderItem[slideIndex].classList.add('active');
 
+    sliderInfoItem[slideIndex].classList.add('active');
+
     slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+
+    sliderInfo.style.transform = `translateX(-${slideIndex * 100}%)`;
 })
